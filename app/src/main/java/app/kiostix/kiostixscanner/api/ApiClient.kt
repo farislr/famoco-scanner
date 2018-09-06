e084a0f417599d5e05ad43333b991c4ca87cc704
farislr <lrfaris24@gmail.com>
2018-09-06 21:46:05 +0700

    update: - change API client link

--- app/src/main/java/app/kiostix/kiostixscanner/api/ApiClient.kt
+++ app/src/main/java/app/kiostix/kiostixscanner/api/ApiClient.kt
@@ -1,8 +1,8 @@
 package app.kiostix.kiostixscanner.api
 
 class ApiClient {
-    private val apiUrlDev = "http://devapi.kiostix.com"
-    private val apiUrl = "http://apiv3.kiostix.com"
+    private val apiUrl = "http://devapi.kiostix.com"
+    private val apiUrlDev = "http://apiv3.kiostix.com"
     val cdn = "http://dev-web.ultraklin.com/api/lini-ultraklin-laravel/public/7.txt"
     val cdnDev = "http://bar_co_de.kiostix.com/EventAccess/Football_AEF_20180812.txt"
     val login = "$apiUrl/login"
