ef311d36b0bebab7d1e30ed7de27733554281115
farislr <lrfaris24@gmail.com>
2019-07-19 16:58:43 +0700

    update on computer

---
+++ app/src/androidTest/java/app/kiostix/kiostixscanner/MainActivityInstrumentedTest.kt
@@ -0,0 +1,20 @@
+package app.kiostix.kiostixscanner
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainActivityInstrumentedTest {
+
+    @Test
+    fun getEmailAuth() {
+    }
+
+    @Test
+    fun onCreateOptionsMenu() {
+    }
+
+    @Test
+    fun onOptionsItemSelected() {
+    }
+}
